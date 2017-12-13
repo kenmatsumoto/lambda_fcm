2b999861832e220f3a1ff791e4ee853ab18cf32b
kenmatsumoto <ken@nri.com>
2017-12-13 12:47:02 -0800

    coffee ordering test

--- index.js
+++ index.js
@@ -9,30 +9,49 @@
     
     // See the "Managing device groups" link above on how to generate a
     // notification key.
-    var topic = "highScores";
-    
+    var topic = "news";
+    var dt = new Date();
     // See the "Defining the message payload" section below for details
     // on how to define a message payload.
     var payload = {
         data: {
-            score: "850",
-            time: "2:45"
+            title: "New coffee order received!",
+            time: dt.toTimeString()
         }
     };
     
     
+    function fcm_send () {
+        return admin.messaging().sendToTopic(topic, payload)
+        .then(function(response) {
+            // See the MessagingDeviceGroupResponse reference documentation for
+            // the contents of response.
+            console.log("Successfully sent message:", response.message);
+        })
+        .catch(function(error) {
+            console.log("Error sending message:", error);
+        });
+    }
     
+
     exports.handler = (event, context, callback) => {
-        
+        context.callbackWaitsForEmptyEventLoop = false;
         // Send a message to the device group corresponding to the provided
         // notification key.
+        
         admin.messaging().sendToTopic(topic, payload)
         .then(function(response) {
             // See the MessagingDeviceGroupResponse reference documentation for
             // the contents of response.
             console.log("Successfully sent message:", response);
+            callback(null, "some success message" + response);
         })
         .catch(function(error) {
             console.log("Error sending message:", error);
+            callback(null, "some error message" + error);
         });
+        
+        //var message = fcm_send();
+        var message = " ok";
+        callback(null, "some success message" + message);
     };
